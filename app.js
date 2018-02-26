5b529082de6682e339a09befa69d484953aa992d
Fawad Khan <fawadkhan03@gmail.com>
2018-02-25 23:53:29 -0500

    add social links and clean up app.js a bit

--- app.js
+++ app.js
@@ -8,11 +8,6 @@ app.get("/", function(req, res){
     res.render("home");
 });
 
-
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The Server Has Started!");
  });
-
-// app.listen(3001, 'localhost', function(){
-//     console.log("server is running");
-// });
