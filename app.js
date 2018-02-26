065da6ca3a0dc56ed25eac569de9a9a4bee0ea82
Fawad Khan <fawadkhan03@gmail.com>
2018-02-26 17:57:34 -0500

    add why happy tots route

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get("/", function(req, res){
     res.render("index");
 });
 
+app.get("/why-happy-tots", function(req, res){
+	res.render("why-happy-tots");
+})
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The Server Has Started!");
  });
