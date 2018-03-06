b240e05cb39076f8c36821d793cba8acb8dfac3b
Fawad Khan <fawadkhan03@gmail.com>
2018-03-06 01:02:38 -0600

    add pages and routes for each of the program pages

--- app.js
+++ app.js
@@ -18,7 +18,39 @@ app.get("/locations", function(req, res){
 
 app.get("/our-programs", function(req, res){
 	res.render("programs/our-programs")
-})
+});
+
+app.get("/our-programs/infant", function(req, res){
+	res.render("programs/infant")
+});
+
+app.get("/our-programs/toddler", function(req, res){
+	res.render("programs/toddler")
+});
+
+app.get("/our-programs/preschool", function(req, res){
+	res.render("programs/preschool")
+});
+
+app.get("/our-programs/transitional-kindergarten", function(req, res){
+	res.render("programs/tk")
+});
+
+app.get("/our-programs/kindergarten", function(req, res){
+	res.render("programs/kindergarten")
+});
+
+app.get("/our-programs/field-trips", function(req, res){
+	res.render("programs/fieldtrips")
+});
+
+app.get("/our-programs/language-classes", function(req, res){
+	res.render("programs/language")
+});
+
+app.get("/our-programs/summer", function(req, res){
+	res.render("programs/summer")
+});
 
 app.get("/enroll", function(req,res){
 	res.render("enroll");
