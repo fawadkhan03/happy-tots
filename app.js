198a8d6888f25ddae4c4e75c3f41e7bb09da6308
Fawad Khan <fawadkhan03@gmail.com>
2018-03-09 13:31:05 -0800

    add body-parser middleware to app.js

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ var programsRoutes = require("./routes/programs");
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get("/", function(req, res){
     res.render("index");
 });
